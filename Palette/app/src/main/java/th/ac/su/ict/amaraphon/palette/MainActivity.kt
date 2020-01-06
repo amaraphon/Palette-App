64941aa34f22861920d050b7d9cc32bdd4319458
amaraphon <57031249+amaraphon@users.noreply.github.com>
2020-01-06 15:04:59 +0700

    Kotlin

--- Palette/app/src/main/java/th/ac/su/ict/amaraphon/palette/MainActivity.kt
+++ Palette/app/src/main/java/th/ac/su/ict/amaraphon/palette/MainActivity.kt
@@ -1,8 +1,10 @@
 package th.ac.su.ict.amaraphon.palette
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,18 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val baseBt      = findViewById<Button>(R.id.baseBt)
-        val pinkBt      = findViewById<Button>(R.id.pinkBt)
-        val dIndigoBt   = findViewById<Button>(R.id.dIndigoBt)
-        val mdIndigoBt  = findViewById<Button>(R.id.mdIndigoBt)
-        val lowIndigoBt = findViewById<Button>(R.id.lowIndigoBt)
-        val blueBt      = findViewById<Button>(R.id.blueBt)
+        var viewColor = findViewById<TextView>(R.id.viewColor)
 
+        var bt1 = findViewById<Button>(R.id.bt1)
+        var bt2 = findViewById<Button>(R.id.bt2)
+        var bt3 = findViewById<Button>(R.id.bt3)
+        var bt4 = findViewById<Button>(R.id.bt4)
+        var bt5 = findViewById<Button>(R.id.bt5)
+        var bt6 = findViewById<Button>(R.id.bt6)
 
-
-
-
-
+        bt1.setOnClickListener(){
+            viewColor.setText("235-255-221")
+            viewColor.setBackgroundColor(Color.parseColor("#ebe1dd"))
+        }
+        bt2.setOnClickListener(){
+            viewColor.setText("236-203-202")
+            viewColor.setBackgroundColor(Color.parseColor("#eccbca"))
+        }
+        bt3.setOnClickListener(){
+            viewColor.setText("117-131-156")
+            viewColor.setBackgroundColor(Color.parseColor("#75839c"))
+        }
+        bt4.setOnClickListener(){
+            viewColor.setText("153-169-191")
+            viewColor.setBackgroundColor(Color.parseColor("#99a9bf"))
+        }
+        bt5.setOnClickListener(){
+            viewColor.setText("195-204-216")
+            viewColor.setBackgroundColor(Color.parseColor("#c3ccd8"))
+        }
+        bt6.setOnClickListener(){
+            viewColor.setText("221-226-269")
+            viewColor.setBackgroundColor(Color.parseColor("#dee2e5"))
+        }
 
     }
 }
